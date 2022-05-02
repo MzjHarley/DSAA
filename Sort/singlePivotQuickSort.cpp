3f5c0fc8e42c2d74c169956b8f33fb51814c02f5
OMNSP <98378474+MzjHarley@users.noreply.github.com>
2022-05-02 16:00:13 +0800

    Update singlePivotQuickSort.cpp

--- Sort/singlePivotQuickSort.cpp
+++ Sort/singlePivotQuickSort.cpp
@@ -3,18 +3,18 @@
 #include<ctime>
 
 using namespace std;
-#define ARR_SIZE 100
+#define ARR_SIZE 20
 
 void Random(int* a,int len);
 void singlePivotQuickSort(int*a,int begin,int end);
-int partition(int*a,int left,int right);
+void partition(int*a,int pivot,int &leftptr,int&rightptr);
 void print(int* a,int len);
 
 int main()
 {
     int a[ARR_SIZE];
     Random(a,ARR_SIZE);
-    singlePivotQuickSort(a,0,ARR_SIZE);
+    singlePivotQuickSort(a,0,ARR_SIZE-1);
     print(a,ARR_SIZE);
     return 0;
 }
@@ -33,25 +33,27 @@ void print(int* a,int len)
 }
 /**
  * 1.patition,based on pivot(the pivot is rightmost number by default) it is divided into two parts,left <= pivot,right > pivot.
- * 2.we use siglePivotQuickSort function to operate on these two parts separately.(begin,pivot's index-1) (pivot's index) (pivot's index +1,end)
+ * 2.we use siglePivotQuickSort function to operate on these two parts separately.(begin,leftptr-1) (leftptr) (leftptr+1,end)
  */ 
 void singlePivotQuickSort(int*a,int begin,int end)
 {
     if(begin<0||end<0||begin>=end) return;
-    int middle = partition(a,begin,end);
-    singlePivotQuickSort(a,begin,middle-1);
-    singlePivotQuickSort(a,middle+1,end);
+    int pivot = a[end],leftptr = begin,rightptr=end-1;
+    partition(a,pivot,leftptr,rightptr);
+    singlePivotQuickSort(a,begin,leftptr-1);
+    singlePivotQuickSort(a,leftptr+1,end);
 }
-int partition(int*a,int left,int right)
+void partition(int*a,int pivot,int& leftptr,int& rightptr)
 {
-    int pivot = a[right],leftptr = left,rightptr=right-1;
+    int right = rightptr+1;//right is pivot's index.
     while(leftptr<=rightptr)
     {
         while(leftptr<=rightptr&&a[leftptr]<=pivot) leftptr++;
         while(leftptr<=rightptr&&a[rightptr]>pivot) rightptr--;
         if(leftptr<rightptr) swap(a[leftptr],a[rightptr]);
     }
-    //above cycle end array status: (left...rightptr) < (leftptr... ) right
+    //above cycle end array status: (left,rightptr)  (leftptr,right-1) right
+    // rightptr = leftptr-1;
     swap(a[leftptr],a[right]);
-    return leftptr;
+    //now the array status :(left,rightptr) leftptr(is pivot) (leftptr+1,right)
 }
